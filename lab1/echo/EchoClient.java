22a6a22ccadf8f37efafdffd5c3ce4b47084ac0b
Zeeshan Qureshi <zee@zqureshi.in>
2013-01-14 00:54:27 -0500

    Convert from DOS to UNIX line delimiters.

--- lab1/echo/EchoClient.java
+++ lab1/echo/EchoClient.java
@@ -2,70 +2,70 @@ import java.io.*;
 import java.net.*;
 
 public class EchoClient {
-	public static void main(String[] args) throws IOException,
-			ClassNotFoundException {
+    public static void main(String[] args) throws IOException,
+            ClassNotFoundException {
 
-		Socket echoSocket = null;
-		ObjectOutputStream out = null;
-		ObjectInputStream in = null;
+        Socket echoSocket = null;
+        ObjectOutputStream out = null;
+        ObjectInputStream in = null;
 
-		try {
-			/* variables for hostname/port */
-			String hostname = "localhost";
-			int port = 4444;
-			
-			if(args.length == 2 ) {
-				hostname = args[0];
-				port = Integer.parseInt(args[1]);
-			} else {
-				System.err.println("ERROR: Invalid arguments!");
-				System.exit(-1);
-			}
-			echoSocket = new Socket(hostname, port);
+        try {
+            /* variables for hostname/port */
+            String hostname = "localhost";
+            int port = 4444;
 
-			out = new ObjectOutputStream(echoSocket.getOutputStream());
-			in = new ObjectInputStream(echoSocket.getInputStream());
+            if(args.length == 2 ) {
+                hostname = args[0];
+                port = Integer.parseInt(args[1]);
+            } else {
+                System.err.println("ERROR: Invalid arguments!");
+                System.exit(-1);
+            }
+            echoSocket = new Socket(hostname, port);
 
-		} catch (UnknownHostException e) {
-			System.err.println("ERROR: Don't know where to connect!!");
-			System.exit(1);
-		} catch (IOException e) {
-			System.err.println("ERROR: Couldn't get I/O for the connection.");
-			System.exit(1);
-		}
+            out = new ObjectOutputStream(echoSocket.getOutputStream());
+            in = new ObjectInputStream(echoSocket.getInputStream());
 
-		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-		String userInput;
+        } catch (UnknownHostException e) {
+            System.err.println("ERROR: Don't know where to connect!!");
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("ERROR: Couldn't get I/O for the connection.");
+            System.exit(1);
+        }
 
-		System.out.print("CONSOLE>");
-		while ((userInput = stdIn.readLine()) != null
-				&& userInput.toLowerCase().indexOf("bye") == -1) {
-			/* make a new request packet */
-			EchoPacket packetToServer = new EchoPacket();
-			packetToServer.type = EchoPacket.ECHO_REQUEST;
-			packetToServer.message = userInput;
-			out.writeObject(packetToServer);
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        String userInput;
 
-			/* print server reply */
-			EchoPacket packetFromServer;
-			packetFromServer = (EchoPacket) in.readObject();
+        System.out.print("CONSOLE>");
+        while ((userInput = stdIn.readLine()) != null
+                && userInput.toLowerCase().indexOf("bye") == -1) {
+            /* make a new request packet */
+            EchoPacket packetToServer = new EchoPacket();
+            packetToServer.type = EchoPacket.ECHO_REQUEST;
+            packetToServer.message = userInput;
+            out.writeObject(packetToServer);
 
-			if (packetFromServer.type == EchoPacket.ECHO_REPLY)
-				System.out.println("echo: " + packetFromServer.message);
+            /* print server reply */
+            EchoPacket packetFromServer;
+            packetFromServer = (EchoPacket) in.readObject();
 
-			/* re-print console prompt */
-			System.out.print("CONSOLE>");
-		}
+            if (packetFromServer.type == EchoPacket.ECHO_REPLY)
+                System.out.println("echo: " + packetFromServer.message);
 
-		/* tell server that i'm quitting */
-		EchoPacket packetToServer = new EchoPacket();
-		packetToServer.type = EchoPacket.ECHO_BYE;
-		packetToServer.message = "Bye!";
-		out.writeObject(packetToServer);
+            /* re-print console prompt */
+            System.out.print("CONSOLE>");
+        }
 
-		out.close();
-		in.close();
-		stdIn.close();
-		echoSocket.close();
-	}
+        /* tell server that i'm quitting */
+        EchoPacket packetToServer = new EchoPacket();
+        packetToServer.type = EchoPacket.ECHO_BYE;
+        packetToServer.message = "Bye!";
+        out.writeObject(packetToServer);
+
+        out.close();
+        in.close();
+        stdIn.close();
+        echoSocket.close();
+    }
 }
--- lab1/echo/EchoServer.java
+++ lab1/echo/EchoServer.java
@@ -7,19 +7,19 @@ public class EchoServer {
         boolean listening = true;
 
         try {
-        	if(args.length == 1) {
-        		serverSocket = new ServerSocket(Integer.parseInt(args[0]));
-        	} else {
-        		System.err.println("ERROR: Invalid arguments!");
-        		System.exit(-1);
-        	}
+            if(args.length == 1) {
+                serverSocket = new ServerSocket(Integer.parseInt(args[0]));
+            } else {
+                System.err.println("ERROR: Invalid arguments!");
+                System.exit(-1);
+            }
         } catch (IOException e) {
             System.err.println("ERROR: Could not listen on port!");
             System.exit(-1);
         }
 
         while (listening) {
-        	new EchoServerHandlerThread(serverSocket.accept()).start();
+            new EchoServerHandlerThread(serverSocket.accept()).start();
         }
 
         serverSocket.close();
