0d649096417e6925ab0914ba17362f2542e54641
Zeeshan Qureshi <zee@zqureshi.in>
2013-01-18 01:29:18 -0500

    Add broker lookup into Client.

--- lab1/broker/broker3/BrokerClient.java
+++ lab1/broker/broker3/BrokerClient.java
@@ -2,38 +2,27 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class BrokerClient {
+    private enum Commands {
+        LOCAL,
+        BYE,
+        EXIT,
+        QUIT;
+    }
+
     public static void main(String args[]) throws IOException, ClassNotFoundException {
+        LookupClient client = null;
         Socket brokerSocket = null;
         ObjectOutputStream out = null;
         ObjectInputStream in = null;
 
-        try {
-            /* variables for hostname/port */
-            String hostname = "localhost";
-            int port = 4444;
-
-            if(args.length == 2 ) {
-                hostname = args[0];
-                port = Integer.parseInt(args[1]);
-            } else {
-                System.err.println("ERROR: Invalid arguments!");
-                System.exit(-1);
-            }
-            brokerSocket = new Socket(hostname, port);
-
-            out = new ObjectOutputStream(brokerSocket.getOutputStream());
-            in = new ObjectInputStream(brokerSocket.getInputStream());
-
-        } catch (UnknownHostException e) {
-            System.err.println("ERROR: Don't know where to connect!!");
-            System.exit(1);
-        } catch (IOException e) {
-            System.err.println("ERROR: Couldn't get I/O for the connection.");
-            System.exit(1);
+        if(args.length == 2 ) {
+            client = new LookupClient(args[0], Integer.parseInt(args[1]));
+        } else {
+            System.err.println("ERROR: Invalid arguments!");
+            System.exit(-1);
         }
 
         Scanner stdIn = new Scanner(System.in);
@@ -43,29 +32,64 @@ public class BrokerClient {
         outside_loop:
         while (stdIn.hasNext()) {
             /* Read input from standard input */
-            userInput = stdIn.next().toLowerCase();
+            userInput = stdIn.next();
 
-            if(userInput.equals("bye")
-                || userInput.equals("exit")
-                || userInput.equals("quit")) {
-                break outside_loop;
-            }
+            try {
+                switch(Commands.valueOf(userInput.toUpperCase())) {
+                case LOCAL:
+                    String exchange = stdIn.next();
+                    BrokerLocation brokerLocation = null;
 
-            /* make a new request packet */
-            BrokerPacket packetToServer = new BrokerPacket();
-            packetToServer.type = BrokerPacket.BROKER_REQUEST;
-            packetToServer.symbol = userInput;
-            out.writeObject(packetToServer);
+                    /* Look up brokers for exchange */
+                    System.out.println("Looking up broker for exchange " + exchange);
+                    try {
+                        if((brokerLocation = client.lookup(exchange)) == null) {
+                           System.out.println("No broker found!");
+                           break;
+                        }
+                    } catch (IOException e) {
+                        System.out.println("Could not connect to lookup server!");
+                        System.exit(-1);
+                    }
 
-            /* print server reply */
-            BrokerPacket packetFromServer;
-            packetFromServer = (BrokerPacket) in.readObject();
+                    if(brokerLocation == null) {
+                        System.err.println("No broker found!");
+                    }
 
-            if (packetFromServer.type == BrokerPacket.BROKER_QUOTE) {
-                if(packetFromServer.error_code == BrokerPacket.ERROR_INVALID_SYMBOL) {
-                    System.out.println("Invalid Symbol.");
+                    /* Connect to broker */
+                    System.out.println("Connecting to broker..." + brokerLocation);
+                    brokerSocket = new Socket(brokerLocation.broker_host, brokerLocation.broker_port);
+                    out = new ObjectOutputStream(brokerSocket.getOutputStream());
+                    in = new ObjectInputStream(brokerSocket.getInputStream());
+                    break;
+
+                case BYE:
+                case EXIT:
+                case QUIT:
+                    break outside_loop;
+                }
+            } catch (IllegalArgumentException e) {
+                /* If no command matched, then must be a Symbol, request quote for it */
+                if(brokerSocket != null) {
+                    /* make a new request packet */
+                    BrokerPacket packetToServer = new BrokerPacket();
+                    packetToServer.type = BrokerPacket.BROKER_REQUEST;
+                    packetToServer.symbol = userInput;
+                    out.writeObject(packetToServer);
+
+                    /* print server reply */
+                    BrokerPacket packetFromServer;
+                    packetFromServer = (BrokerPacket) in.readObject();
+
+                    if (packetFromServer.type == BrokerPacket.BROKER_QUOTE) {
+                        if(packetFromServer.error_code == BrokerPacket.ERROR_INVALID_SYMBOL) {
+                            System.out.println("Invalid Symbol.");
+                        } else {
+                            System.out.println("Quote: " + packetFromServer.quote);
+                        }
+                    }
                 } else {
-                    System.out.println("Quote: " + packetFromServer.quote);
+                    System.out.println("Must be connected to a broker first! Use 'local' command to connect.");
                 }
             }
 
